3e15256c07f41acd3e22a31f89201e7dd24918ea
Petur Georgiev <67598390+pepii99@users.noreply.github.com>
2022-07-26 04:28:23 +0300

    Delete PizzaStore directory

--- PizzaStore/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PizzaStore.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
